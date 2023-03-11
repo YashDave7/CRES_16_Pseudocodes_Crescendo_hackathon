3bac307fc57c8571772c00c418f99e9cbfc3d12b
moheet333 <moheetshendarkar@gmail.com>
2023-03-11 13:42:27 +0530

    Added little mongoDB :)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 // EJS.
@@ -9,7 +11,13 @@ app.set('view engine','ejs');
 
 // BodyParser.
 app.use(express.urlencoded( {extended: false} ));
+app.use(express.json());
 
+// MongoDB starts
+
+mongoose.connect(process.env.MONGODB);
+
+// MongoDB ends
 
 
 const PORT = process.env.PORT || 5000;
---
+++ models/patientModel.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const patientModel = new mongoose.Schema({
+    
+})
