a1f39f7a2bf642aec77ee190dd80848ab9b4ac39
moheet333 <moheetshendarkar@gmail.com>
2023-03-11 14:37:54 +0530

    Database Connected

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const patientModel = require("./models/patientModel");
 const app = express();
-kedhfesklllllllllll
+require("dotenv").config();
+
 // EJS.
 app.use(expressLayouts);
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 // BodyParser.
-app.use(express.urlencoded( {extended: false} ));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 // MongoDB starts
 
 mongoose.connect(process.env.MONGODB);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Successfully connected Database");
+});
 
 // MongoDB ends
-
 
 const PORT = process.env.PORT || 5000;
 // Listen to the Port.
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
--- models/patientModel.js
+++ models/patientModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
-const patientSchema = new mongoose.Schema({});
+const patientSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
 
 const patientModel = mongoose.model("patientModel", patientSchema);
 module.exports = patientModel;
